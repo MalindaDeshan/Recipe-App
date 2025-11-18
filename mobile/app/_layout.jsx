e14facfa65c5db3094484496c7d48d1d52d73dfc
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-18 03:25:42 -0800

    fix layout.jsx

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,11 +1,11 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Slot />
     </ClerkProvider>
   );
 }
