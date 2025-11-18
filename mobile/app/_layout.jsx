4149750f37380f9a2c50da6d22afaba58cc57461
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-18 03:22:25 -0800

    Setup authentication

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ClerkProvider>
+  );
+}
+
