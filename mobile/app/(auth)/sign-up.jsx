fcd62668d72dd2b3ba0d0563209d3edb5f5d23d6
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-20 03:30:46 -0800

    Sign-Up page completed

--- mobile/app/(auth)/sign-up.jsx
+++ mobile/app/(auth)/sign-up.jsx
@@ -1,7 +1,12 @@
 import { useSignUp } from '@clerk/clerk-expo';
 import { useRouter } from 'expo-router'
 import React, { Component, useState } from 'react'
-import { Alert, Text, View } from 'react-native'
+import { Alert, KeyboardAvoidingView, Platform, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { authStyles } from '../../assets/styles/auth.styles';
+
+import { Image } from 'expo-image';
+import { COLORS } from '../../constants/colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const SignupScreen = () => {
 
@@ -37,9 +42,96 @@ const SignupScreen = () => {
 
   };
 
+    if (pendingVerification) return "pending ui will go here";
+
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={authStyles.container}>
+        <KeyboardAvoidingView 
+          behavior={Platform.os === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.select({
+                  ios: 64,
+                  android: 50,
+                })}
+          style={authStyles.keyboardView}
+        >
+
+          <ScrollView
+          contentContainerStyle={authStyles.scrollContent}
+          showsHorizontalScrollIndicator={false}
+          >
+            <View style={authStyles.imageContainer}>
+                        <Image 
+                          source={require("../../assets/images/i2.png")}
+                          style={authStyles.image}
+                          contentFit='contain'
+                        />
+                      </View>
+            <Text style={authStyles.title}>Create Account</Text>
+
+            <View style={authStyles.formContainer}>
+              {/* EMAIL INPUT */}
+              <View style={authStyles.inputContainer}>
+                <TextInput
+                  style={authStyles.textInput}
+                  placeholder="Enter Email"
+                  placeholderTextColor={COLORS.textLight}
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+              </View>
+
+              {/* PASSWORD INPUT */}
+              <View style={authStyles.inputContainer}>
+                <TextInput
+                  style={authStyles.textInput}
+                  placeholder="Enter Password"
+                  placeholderTextColor={COLORS.textLight}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                  autoCapitalize="none"
+                />
+                <TouchableOpacity
+                  style={authStyles.eyeButton}
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Ionicons 
+                    name={showPassword ? "eye-outline" : "eye-off-outline"} 
+                    size={24} 
+                    color={COLORS.textLight} 
+                  />
+
+                </TouchableOpacity>
+              </View>
+
+              {/* SIGN UP BUTTON */}
+              <TouchableOpacity
+                  style={[authStyles.authButton, loading && authStyles.buttonDisabled]}
+                  onPress={handleSignUp}
+                  disabled={loading}
+                  activeOpacity={0.8}
+              >
+              <Text style={authStyles.buttonText}>
+                {loading ? "Creating Account..." : "Sign Up"}
+              </Text>
+              </TouchableOpacity>
+
+              {/*Sign Up Link*/}
+              <TouchableOpacity
+                style={authStyles.linkContainer}
+                onPress={()=> router.back()}
+              >
+                <Text style={authStyles.linkText}>
+                  Already have an account?<Text style={authStyles.link}>Sign In</Text>
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+          </ScrollView>
+        </KeyboardAvoidingView>
+       
       </View>
     )
 
