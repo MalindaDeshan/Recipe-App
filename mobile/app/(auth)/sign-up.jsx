2255ecb63ee24d1172927228b26ea7e2d762ed49
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-20 03:07:12 -0800

    sign-up page functions completed

--- mobile/app/(auth)/sign-up.jsx
+++ mobile/app/(auth)/sign-up.jsx
@@ -1,7 +1,7 @@
 import { useSignUp } from '@clerk/clerk-expo';
 import { useRouter } from 'expo-router'
 import React, { Component, useState } from 'react'
-import { Text, View } from 'react-native'
+import { Alert, Text, View } from 'react-native'
 
 const SignupScreen = () => {
 
@@ -13,6 +13,29 @@ const SignupScreen = () => {
   const [loading,setLoading] = useState(false);
   const [pendingVerification , setPendingVerification] = useState(false);
 
+  const handleSignUp = async () => {
+    if (!email || !password) return Alert.alert("Error","Please fill in all fields");
+    if (password.length<6) return Alert.alert("Error","Password must be at least 6 characters");
+
+    if(!isLoaded) return;
+
+    setLoading(true)
+
+    try {
+      await signUp.create({emailAddress:email,password})
+
+      await signUp.prepareEmailAddressVerification({strategy: "email_code"})
+
+      setPendingVerification(true)
+
+    } catch (error) {
+      Alert.alert("Error",error.errors?.[0]?.message || "Failed to create account");
+      console.error(JSON.stringify(error,null,2));
+    } finally {
+      setLoading(false)
+    }
+
+  };
 
     return (
       <View>
