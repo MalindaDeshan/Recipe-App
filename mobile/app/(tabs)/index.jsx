111a452aad9f146421d3f0d8164f29a2a2b77a3b
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-21 14:38:51 -0800

    fetch data logics from MealDB API created completely

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,7 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router';
 
 const HomeScreen = () => {
+
+    const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [featuredRecipe, setFeaturedRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
   return (
     <View>
       <Text>HomeScreen</Text>
