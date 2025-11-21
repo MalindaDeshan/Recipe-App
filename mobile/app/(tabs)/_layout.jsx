4d6897ccd957f731611130a62b718df9dcf43d6d
Malinda Deshan <126862086+MalindaDeshan@users.noreply.github.com>
2025-11-21 13:52:00 -0800

    Tab navigation bar completed

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Redirect, Stack } from 'expo-router'
+import { Redirect, Stack, Tabs } from 'expo-router'
 import { useAuth } from '@clerk/clerk-expo'
+import iconSet from '@expo/vector-icons/build/Fontisto';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '../../constants/colors';
 
 const TabsLayout = () => {
     const {isSignedIn} = useAuth();
@@ -9,7 +12,49 @@ const TabsLayout = () => {
         return <Redirect href={'/(auth)/sign-in'}/>
     }
   return (
-     <Stack/>
+     <Tabs
+      screenOptions={{
+        headerShown:false,
+        tabBarActiveTintColor:COLORS.primary,
+        tabBarInactiveTintColor:COLORS.textLight,
+        tabBarStyle : {
+          backgroundColor:COLORS.white,
+          borderTopColor:COLORS.border,
+          borderTopWidth:1,
+          paddingBottom:8,
+          paddingTop:8,
+          height:80,
+        },
+        tabBarLabelStyle:{
+          fontSize:12,
+          fontWeight:"600"
+        }
+      }}
+     >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title:"Recipes",
+          tabBarIcon:({color,size}) => <Ionicons name = "restaurant" size={size} color={color}/>
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title:"Search",
+          tabBarIcon:({color,size}) => <Ionicons name ="search" size={size} color={color}/>
+        }}
+        />
+      
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title:"Favorite",
+          tabBarIcon:({color,size}) => <Ionicons name ="heart" size={size} color={color}/>
+        }}
+        />
+     </Tabs>
   )
 }
 
